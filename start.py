17cb4d872c18151582f1d3fe4453e41a8e999af8
raymag <prayk11@outlook.com>
2020-06-19 16:48:29 -0300

    updated 'stats' command. Close #6
    It now has a pagination system on it

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import requests
 import time
 import json
 import math
+import asyncio
 
 from discord.ext import commands
 
@@ -70,38 +71,46 @@ async def stats(ctx, mode, display_time = 0, limit = 50000 ):
     channel = bot.get_channel(715142746356187195)
     
     msg = "Modo incorreto, vai para Campo Alegre rapaz."
+    msg_title = ""
+    current_page = 1
+    total_pages = 1
+    items_per_page = 50
+    array = []
+    channel = ctx.channel
 
-    def render_msg_list(array, msg = ""):
+
+    def render_msg_list(array, msg = "", page=1, items_per_page=5):
+        subarray = array[ ((current_page * items_per_page) - items_per_page ) : ((current_page * items_per_page) - 1) ]
         counter = 1
-        for item in array:
-            msg += str(counter) + "º) " + str(item) + ": " + str(array[item]) + "\n"
+        if current_page > 1:
+            counter = (current_page * items_per_page) - items_per_page
+        for item in subarray:
+            msg += str(counter) + ". " + str(item[0]) + ": " + str(item[1]) + "\n"
             counter += 1
         return msg
     
     if mode == "messages":
         users = {}
-        msg = "**Number of messages per author:**\n\n"
+        msg_title = "**Number of messages per author:**\n\n"
         
         async for message in channel.history(limit = limit):
-            if (message.author.name not in users):
-                users[message.author.name] = 1
+            if (message.author.mention not in users):
+                users[message.author.mention] = 1
             
             else:
-                users[message.author.name] += 1
+                users[message.author.mention] += 1
             
-        sorted_users = sorted(users.items(), key=lambda kv: kv[1], reverse = True)
-        users = collections.OrderedDict(sorted_users)
+        #users
+        array = sorted(users.items(), key=lambda kv: kv[1], reverse = True)
             
         current_page = 1
-        total_pages = math.ceil(len(users)/50)
-
-        msg = render_msg_list(users, msg)
+        total_pages = math.ceil(len(array)/items_per_page)
         
             
     elif mode == "emojis":
         emojis = {i : 0 for i in bot.emojis}
         
-        msg = "**Number of times that each emoji was used\n\n**"
+        msg_title = "**Number of times that each emoji was used\n\n**"
         
         async for message in channel.history(limit = limit):
             for reaction in message.reactions:
@@ -111,22 +120,90 @@ async def stats(ctx, mode, display_time = 0, limit = 50000 ):
                     except:
                         continue                        
 
-        sorted_emojis = sorted(emojis.items(), key=lambda kv: kv[1], reverse = True)
-        emojis = collections.OrderedDict(sorted_emojis)
+        #emojis
+        array = sorted(emojis.items(), key=lambda kv: kv[1], reverse = True)
 
-        msg = render_msg_list(emojis, msg)
-            
+        current_page = 1
+        total_pages = math.ceil(len(array)/items_per_page)
+
     else:
         print("Usage: stats emojis/messages <-time>")
-            
+
     total_time = time.time() - start_time
+
+    async def show_stats(ctx, array, current_page, total_pages, items_per_page, msg_title, user):
+
+        def render_msg_list(array, msg = "", page=1, items_per_page=5):
+            subarray = array[ ((current_page * items_per_page) - items_per_page ) : ((current_page * items_per_page) - 1) ]
+            counter = 1
+            if current_page > 1:
+                counter = (current_page * items_per_page) - items_per_page
+            for item in subarray:
+                msg += str(counter) + ". " + str(item[0]) + ": " + str(item[1]) + "\n"
+                counter += 1
+            return msg
+
+        msg = render_msg_list(array, msg_title, current_page, items_per_page) 
     
-    embed = discord.Embed(
-        title = "",
-        description = msg[0:1990],
-        color = 0xeee657
-    )
-    await ctx.send(embed = embed)
+        embed = discord.Embed(
+            title = "",
+            description = msg[0:1990],
+            color = 0xeee657
+        )
+        embed.add_field(name = 'Page', value = '{}/{}'.format(current_page, total_pages))
+        stats = await ctx.send(embed = embed)
+
+        react_emojis = ['◀️', '▶️']
+        for emoji in react_emojis:
+            await stats.add_reaction(emoji)
+
+        def check_react(reaction, user):
+                if reaction.message.id != stats.id:
+                    return False
+                if str(reaction.emoji) not in react_emojis:
+                    return False
+                if user != ctx.message.author:
+                    return False
+                return True
+        try:
+            res, user = await bot.wait_for('reaction_add', check=check_react, timeout=60)
+        except asyncio.TimeoutError:
+            # return await stats.clear_reactions()
+            pass
+        
+        if user != ctx.message.author:
+            pass
+        elif '◀️' in str(res.emoji) and current_page > 1:
+            print('Previous page')
+            current_page -= 1
+            msg = render_msg_list(array, msg_title, current_page, items_per_page)
+
+            embed = discord.Embed(
+                title = "",
+                description = msg[0:1990],
+                color = 0xeee657
+            )
+            embed.add_field(name = 'Page', value = '{}/{}'.format(current_page, total_pages))
+
+            # await stats.clear_reactions()
+            await show_stats(ctx, array, current_page, total_pages, items_per_page, msg_title, user)
+        elif '▶️' in str(res.emoji) and current_page < total_pages:
+            print('Next page')
+            current_page += 1
+            msg = render_msg_list(array, msg_title, current_page, items_per_page)
+
+            embed = discord.Embed(
+                title = "",
+                description = msg[0:1990],
+                color = 0xeee657
+            )
+            embed.add_field(name = 'Page', value = '{}/{}'.format(current_page, total_pages))
+
+            # await stats.clear_reactions()
+            await show_stats(ctx, array, current_page, total_pages, items_per_page, msg_title, user)
+    
+
+    await show_stats(ctx, array, current_page, total_pages, items_per_page, msg_title, ctx.message.author)        
     
     if (display_time == "-time"):
         await ctx.send("Stats completed in " + str(total_time) + " seconds")
